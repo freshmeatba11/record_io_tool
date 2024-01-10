210a98ea952b2c334d3a39067f5f952880ffd7a8
Jason Chen <cmjj8898212032@gmail.com>
2024-01-10 15:51:23 +0800

    feat: add homepage footer image.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,30 @@ import React, { useState } from "react";
 
 import FrontTitle from "@/components/frontTitle";
 import LoginInputArea from "@/components/input/loginInputArea";
-// import MedWorkerSvg from "@/assets/images/medical_workers.svg";
+import MedWorkerSvg from "@/assets/images/medical_workers.svg";
 
 const Wrapper = styled.main`
   width: 100%;
   height: 100%;
   padding: 60px 0 0;
+
+  position: relative;
+`;
+const SvgWrapper = styled.div`
+  margin: 0 auto;
+  max-width: 390px;
+  max-height: 318px;
+
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  left: 0;
+
+  z-index: -1;
+  svg {
+    width: 100%;
+    height: 100%;
+  }
 `;
 
 export default function Home() {
@@ -18,8 +36,10 @@ export default function Home() {
       <FrontTitle title1="IO" title2="Recording" subTitle="輸出入量紀錄" />
 
       <LoginInputArea />
-      {/* //todo image */}
-      {/* <MedWorkerSvg /> */}
+
+      <SvgWrapper>
+        <MedWorkerSvg />
+      </SvgWrapper>
     </Wrapper>
   );
 }
