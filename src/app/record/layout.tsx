ad71993813e405dd6a126b5adca501e7ebef9478
Jason Chen <cmjj8898212032@gmail.com>
2024-01-29 14:32:47 +0800

    feat: 1. add new route: record page. 2. move css variable out from darkmode.

---
+++ src/app/record/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const RecordLayout = ({ children }: { children: React.ReactNode }) => {
+  //   todo 上方有logo 檔案資料
+  //   todo 下方有新增按鈕 點擊開啟modal
+  return <div>{children}</div>;
+};
+
+export default RecordLayout;
---
+++ src/app/record/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const RecordPage = () => {
+  //  todo 這裡負責顯示表格
+  return <div>Record</div>;
+};
+
+export default RecordPage;
